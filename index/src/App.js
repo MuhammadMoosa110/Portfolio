ca2e5e36bfe81aeedd58fd402b06370f9536f78d
MuhammadMoosa110 <Moosay50000@gmail.com>
2023-11-27 13:00:14 -0800

    first commit

--- index/src/App.js
+++ index/src/App.js
@@ -32,7 +32,8 @@ function Index() {
         <div className='blur'></div><div className='blur1'></div>
 
 
-          <img src={Img} /><div className='blur2'></div>
+          <img src={Img} />
+          {/* <div className='blur2'></div> */}
           <h1>MUHAMMAD</h1>
           <h3>MOOSA</h3>
           <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="white" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
@@ -42,14 +43,25 @@ function Index() {
 
         <div className='page2'>
           <div id='pg2'><h1>ABOUT</h1>
-          <a href='#pg2'>   <svg onClick={click}
+          <a href='#pg3'>   <svg onClick={click}
          xmlns="http://www.w3.org/2000/svg" width="40" height="30" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
   <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5"/>
 </svg></a>
           
           </div>
         </div>
-        <div className='page3'></div>
+        <div id='pg3' className='page3'>
+        <a href='#pg2'> <svg className='svg1' xmlns="http://www.w3.org/2000/svg" width="60" height="30" fill="currentColor" class="bi bi-arrow-90deg-left" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.146 4.854a.5.5 0 0 1 0-.708l4-4a.5.5 0 1 1 .708.708L2.707 4H12.5A2.5 2.5 0 0 1 15 6.5v8a.5.5 0 0 1-1 0v-8A1.5 1.5 0 0 0 12.5 5H2.707l3.147 3.146a.5.5 0 1 1-.708.708l-4-4z"/>
+</svg></a>
+
+<h1>PROJECTS</h1>
+
+<svg className='svg2' xmlns="http://www.w3.org/2000/svg" width="60" height="30" fill="currentColor" class="bi bi-chevron-double-down" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.646 6.646a.5.5 0 0 1 .708 0L8 12.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+  <path fill-rule="evenodd" d="M1.646 2.646a.5.5 0 0 1 .708 0L8 8.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
+</svg>
+        </div>
       </div>
     </>
   );

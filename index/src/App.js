f5e49bc6dcb45deed8295aec21f285dfd5f5ac7d
MuhammadMoosa110 <Moosay50000@gmail.com>
2023-12-02 02:40:05 -0800

    first commit

--- index/src/App.js
+++ index/src/App.js
@@ -27,28 +27,51 @@ function Index() {
 
           { show && (<PageTwo style={{width:"100%",position:"absolute",Top:"150%"}}/>)}
             <a href='#pg2'>   <svg onClick={showCom}
-              xmlns="http://www.w3.org/2000/svg" width="80" height="50" fill="currentColor" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
+              xmlns="http://www.w3.org/2000/svg" width="80" height="50" fill="White" class="bi bi-arrow-bar-right" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M6 8a.5.5 0 0 0 .5.5h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L12.293 7.5H6.5A.5.5 0 0 0 6 8m-2.5 7a.5.5 0 0 1-.5-.5v-13a.5.5 0 0 1 1 0v13a.5.5 0 0 1-.5.5" />
             </svg></a>
 
           </div>
-          <div className='blur'></div><div className='blur1'></div>
+          
+          <div className='blur'>
+           
+            <h1>W</h1>
+            <br/>
+            <h1>E</h1>
+            <br/>
+            <h1>B</h1>
+
+            <br/>
+            <br/>
+            <h1>D</h1><br/>
+            <h1>E</h1><br/>
+            <h1>V</h1><br/>
+            <h1>E</h1><br/>
+            <h1>L</h1><br/>
+            <h1>O</h1><br/>
+            <h1>P</h1><br/>
+            <h1>E</h1><br/>
+            <h1>R</h1>
+
+   
+          
+            </div> 
 
 
-          <img src={Img} />
-          {/* <div className='blur2'></div> */}
+
+ {/* <div className='blur2'></div> */}
           <h1>MUHAMMAD</h1>
           <h3>MOOSA</h3>
 
-          <div className='Resume' style={{ display: "flex",position:"relative", marginLeft: "50%", marginTop: "4.2%" }}>
+          <div className='Resume'>
             <a download={Resume} href={Resume}> <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="white" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5m-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5" />
             </svg> </a>
-            <h5 style={{ paddingTop: "10%",  paddingLeft: "12%",position:"absolute", color: "white",fontSize:"2vmax", fontFamily: "Orbitron" }}>RESUME</h5>
+            <h5>RESUME</h5>
           </div>
         </div>
  
-          
+        <img src={Img} />
       </div>
       </AnimatePresence>
     </>

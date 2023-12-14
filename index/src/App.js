12c29e44b5a73d68326e7f5dd2d0d91acf56af7a
MuhammadMoosa110 <Moosay50000@gmail.com>
2023-12-14 08:29:04 -0800

    first commit

--- index/src/App.js
+++ index/src/App.js
@@ -15,6 +15,8 @@ function Index() {
       setShow(true)
     }
   }
+ 
+  
   return (
     <>
 
@@ -60,15 +62,20 @@ function Index() {
 
 
  {/* <div className='blur2'></div> */}
-          <h1>MUHAMMAD</h1>
-          <h3>MOOSA</h3>
+          <motion.h1 initial={{opacity:0,x:-0}} animate={{opacity:1,x:-100}} >MUHAMMAD</motion.h1>
+          <motion.h3 initial={{opacity:0,x:0}} animate={{opacity:1,x:-100}} >MOOSA</motion.h3>
 {/* <div> <img src={Img} /> </div> */}
-          <div className='Resume'>
+          <motion.div initial={{opacity:0,y:-5}} animate={{opacity:1,y:20}} transition={{
+  ease: "linear",
+  duration: 2,
+  x: { duration: 1 }
+}}
+           className='Resume'>
             <a download={Resume} href={Resume}> <svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" fill="white" class="bi bi-arrow-bar-up" viewBox="0 0 16 16">
               <path fill-rule="evenodd" d="M8 10a.5.5 0 0 0 .5-.5V3.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 3.707V9.5a.5.5 0 0 0 .5.5m-7 2.5a.5.5 0 0 1 .5-.5h13a.5.5 0 0 1 0 1h-13a.5.5 0 0 1-.5-.5" />
             </svg> </a>
             <h5>RESUME</h5>
-          </div>
+          </motion.div>
         </div>
  
          

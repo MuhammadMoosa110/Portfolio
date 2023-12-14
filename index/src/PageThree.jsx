d38aa6591a9d69ab01d5b34bc6d849034356e9ee
MuhammadMoosa110 <Moosay50000@gmail.com>
2023-12-14 04:30:59 -0800

    first commit

--- index/src/PageThree.jsx
+++ index/src/PageThree.jsx
@@ -1,4 +1,5 @@
 import Style from './App.css'
+import Img from './asset/Pic 1.PNG'
 function PageThree() {
     return ( 
         <>
@@ -9,10 +10,17 @@ function PageThree() {
            
 
             <div className='mainCard'>
-                <div className='card'></div>
-                <div className='card1'></div>
-                <div className='card2'></div>
-                <div className='card'></div>
+                 <div className='card1'>
+                    
+                    <a target='blank' href='https://www.figma.com/proto/c1N9POg2QYxrUWdUOlk8N2/Untitled?node-id=24-12&starting-point-node-id=24%3A12&scaling=scale-down'><h1> UI DESIGN TWO </h1></a>
+                     
+                 </div>
+                <div className='card2'>
+                <a target='blank' href='https://www.figma.com/proto/CtffssT9YMNzRNkTmsypTH/Travel-Agency?type=design&node-id=1-2&t=Ou1pTe61DprBBaf6-0&scaling=scale-down&page-id=0%3A1&starting-point-node-id=1%3A2'><h1> UI DESIGN THREE </h1></a>
+                </div>
+                <div className='card'>
+                <a target='blank' href='https://www.figma.com/proto/c1N9POg2QYxrUWdUOlk8N2/Untitled?node-id=24-12&starting-point-node-id=24%3A12&scaling=scale-down'><h1> UI DESIGN ONE </h1></a>
+                </div>
             </div>
         </div> 
 
